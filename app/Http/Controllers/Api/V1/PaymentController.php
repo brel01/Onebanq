1a85ada141579d4ef9583d0b4755f763814b6603
Tech Bre <77964339+brel01@users.noreply.github.com>
2023-12-19 12:29:05 +0100

    Update PaymentController.php

--- app/Http/Controllers/Api/V1/PaymentController.php
+++ app/Http/Controllers/Api/V1/PaymentController.php
@@ -9,14 +9,11 @@ use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Support\Facades\Http;
 
-
 use Flutterwave\Controller\PaymentController as FWPaymentController;
 use Flutterwave\EventHandlers\ModalEventHandler as PaymentHandler;
 use Flutterwave\Flutterwave;
 use Flutterwave\Library\Modal;
 use Flutterwave\Util\Currency;
-
-
 
 class PaymentController extends Controller
 {
@@ -27,16 +24,16 @@ class PaymentController extends Controller
             $validatedData = $request->validate([
                 'amount' => 'required|numeric',
             ]);
-            
+
             $user = auth('api')->user();
-    
-            // Create a new transaction in the database 
+
+            // Create a new transaction in the database
             $transaction = Transaction::create([
                 'user_id' => $user->id,
                 'amount' => $validatedData['amount'],
                 'status' => 'initiated',
             ]);
-    
+
             // Payment data
             $paymentData = [
                 "amount" => $validatedData['amount'],
@@ -50,31 +47,34 @@ class PaymentController extends Controller
                     ],
                 ],
             ];
-    
+
             // Initialize Flutterwave payment
             $accountPayment = Flutterwave::create("account");
-    
+
             // Create customer object
             $customerObj = $accountPayment->customer->create([
                 "full_name" => $user->name,
                 "email" => $user->email,
                 "phone" => $user->phone,
             ]);
-    
+
             $paymentData['customer'] = $customerObj;
-    
+
             // Create payment payload
             $payload = $accountPayment->payload->create($paymentData);
-    
+
             // Initiate payment
             $result = $accountPayment->initiate($payload);
-    
+
+            // Update transaction status based on payment result
             if ($result['status'] === 'success') {
+                $transaction->update(['status' => 'completed']);
                 return response()->json([
                     'message' => 'Payment successful',
                     'transaction_id' => $result['data']['id'],
                 ], 200);
             } else {
+                $transaction->update(['status' => 'failed']);
                 return response()->json([
                     'error' => 'Payment failed',
                     'reason' => $result['message'],
@@ -87,5 +87,5 @@ class PaymentController extends Controller
             ], 500);
         }
     }
-
 }
+?>
